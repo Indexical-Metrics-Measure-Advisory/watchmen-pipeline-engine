1ea32225b0983e4caf2082a35c69e76a0a01d70d
luke0623 <luke0623@outlook.com>
2021-12-16 21:23:07 +0800

    add main test code

---
+++ main.py
@@ -0,0 +1,4 @@
+from pipeline.index import trigger_pipeline
+
+if __name__ == '__main__':
+    trigger_pipeline("tst","da","da")
--- pipeline/index.py
+++ pipeline/index.py
@@ -9,3 +9,5 @@ log = logging.getLogger("app." + __name__)
 
 def trigger_pipeline(topic_name, instance, trigger_type: TriggerType, current_user=None, trace_id=None):
     trigger_pipeline_2(topic_name, instance, trigger_type, current_user, trace_id)
+
+
